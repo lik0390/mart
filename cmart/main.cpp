461dfb9f05be78f02739cb2042f382e58620a9f1
InkyuLim <dlsrb0390@naver.com>
2018-11-07 12:26:29 +0900

    add : mode function

--- cmart/main.cpp
+++ cmart/main.cpp
@@ -1,8 +1,5 @@
 #include "myfunc.h"
-#define NORMAL_MODE 0
-#define MENU_MODE 1
-#define CALC_MODE 2
-#define TOTAL_MODE 3
+
 
 unsigned int tcount = 0, hou = 0, min = 0, sec = 0;
 int mode = NORMAL_MODE;
@@ -22,12 +19,34 @@ ISR(TIMER0_OVF_vect)
 
 int main(void)
 {
+	initsystem();
+	unsigned char inputkey = 0;
     while (1) 
     {
+		inputkey = getKey();
 		if(mode == NORMAL_MODE)
 		{
 			displayClock(hou, min, sec);
+			if (inputkey == Menu) mode = MENU_MODE;
 		}	
-    }
+		else if (mode == MENU_MODE)
+		{
+			lcd_putsf(0,0,(unsigned char *)"1: Calculation  ");
+			lcd_putsf(0,1,(unsigned char *)"2: Total Sales  ");
+			if(inputkey == key1) mode  = CALC_MODE;
+			else if(inputkey == key2) mode  = TOTAL_MODE;
+			inputkey = 0;
+			_delay_ms(50);
+		}
+		else if (mode == CALC_MODE)
+		{
+			lcd_putsf(0,0,(unsigned char *)"Calculation Mode  ");	
+		}
+		else if (mode == TOTAL_MODE)
+		{
+			lcd_putsf(0,0,(unsigned char *)"Total Sales Mode  ");
+		}
+		if(inputkey == lobby) mode = NORMAL_MODE;
+	}
 }
 
